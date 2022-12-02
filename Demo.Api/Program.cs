6b1f70eaed03c4cab0359c087675f481f59eade9
MUHAMMET FATİH ÖZATA <muhammet.ozata@std.antalya.edu.tr>
2022-12-02 23:59:07 +0300

    -

--- Demo.Api/Program.cs
+++ Demo.Api/Program.cs
@@ -12,32 +12,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection(key: "JwtConfig"));
+//builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection(key: "JwtConfig"));
 
-builder.Services.AddAuthentication(configureOptions: options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme=JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(jwt =>
-{
-    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection(key: "JwtConfig:Secret").Value);
-    jwt.SaveToken = true;
-    jwt.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer= false,
-        ValidateAudience=false,
-        RequireExpirationTime=false,
-        ValidateLifetime=true,
+//builder.Services.AddAuthentication(configureOptions: options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultScheme=JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
+//})
+//.AddJwtBearer(jwt =>
+//{
+//    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection(key: "JwtConfig:Secret").Value);
+//    jwt.SaveToken = true;
+//    jwt.TokenValidationParameters = new TokenValidationParameters()
+//    {
+//        ValidateIssuerSigningKey = true,
+//        IssuerSigningKey = new SymmetricSecurityKey(key),
+//        ValidateIssuer= false,
+//        ValidateAudience=false,
+//        RequireExpirationTime=false,
+//        ValidateLifetime=true,
 
         
-    };
+//    };
 
-})
-;
+//})
+//;
 
 var app = builder.Build();
 
@@ -57,7 +57,7 @@ app.UseCors(builder =>
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
+//app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
